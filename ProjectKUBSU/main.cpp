c502519fee126e3d1017effccd9b7ab6641fd7fd
tautau <tautau@DESKTOP-9IR1S0A>
2022-01-17 10:35:12 +0300

    Добавьте файлы проекта.

---
+++ ProjectKUBSU/main.cpp
@@ -0,0 +1,14 @@
+#include <conio.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	cout << "HelloWorld!" << endl; //выводит в поток
+
+	cout << "Привет, мир!" << endl; //выводит в поток (на русском)
+
+}
